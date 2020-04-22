43d94aacaf11b431a82becf430194e6ca85ed5bb
Kevin Changoluisa <46755578+KevinChangoluisa@users.noreply.github.com>
2020-04-22 01:05:39 -0500

    Update callback2.js

--- callback2.js
+++ callback2.js
@@ -1,17 +1,3 @@
-//callback2
-let empleado = [{
-        id: 1,
-        nombre: "Santiago"
-    },
-    {
-        id: 2,
-        nombre: "Wendy"
-    },
-    {
-        id: 3,
-        nombre: "Leo"
-    }
-];
 // con corchetes se crea vectores en js
 
 let salario = [{
@@ -31,14 +17,33 @@ let getEmpleado = (id, callback) => {
     //Si empleado.id es igual a la id ingresada retornara y guardara el valor
     //en empleadoDB
     if (!empleadoDB) {
-        callback(`El usuario con id: ${id} no existe`);
+        callback(`El empleado con id: ${id} no existe`);
     } else {
         callback(null, empleadoDB);
     }
 }
-getEmpleado(1, (err, usuario) => {
+let getSalario = (empleado, callback) => {
+    let salarioDB = salario.find(salario => salario.id === empleado.id);
+    if (!salarioDB) {
+        callback(`El empleado ${empleado.nombre} no tiene salario`);
+    } else {
+        callback(null, {
+            nombre: empleado.nombre,
+            salario: salarioDB.salario
+        });
+    }
+}
+
+getEmpleado(2, (err, empleado) => {
     if (err) {
         return console.log(err);
     }
-    console.log(usuario);
+    getSalario(empleado, (err2, salario) => {
+        if (err2) {
+            return console.log(err2);
+        }
+        console.log(salario);
+    });
+
+
 });
